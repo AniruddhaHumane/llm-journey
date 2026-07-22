f2da5186b546d291488bb8fb26fd5b096fd38052
AniruddhaHumane <ani.humane@outlook.com>
2026-07-22 23:04:26 +0900

    night 3: multi headed attention

--- night_2.py
+++ night_2.py
@@ -4,12 +4,14 @@ import numpy as np
 import tiktoken
 from transformers import AutoModel
 import matplotlib.pyplot as plt
-np.random.seed(0) 
+
+np.random.seed(0)
 
 SENTENCE = "red hat looks good"
 d_k = 128
 enc = tiktoken.encoding_for_model("gpt-2")
 model = AutoModel.from_pretrained("gpt2")
+
 
 def get_tokens(sentence: str):
     tokens = enc.encode(sentence)
@@ -20,10 +22,12 @@ def get_tokens(sentence: str):
     # print(tokens2)
     # tokens and tokens2 are exactly the same
 
+
 def get_embeddings(tokens):
     embeddings = model.get_input_embeddings().weight[tokens]
     print(embeddings.shape)
     return embeddings
+
 
 tokens = get_tokens(SENTENCE)
 embeddings = get_embeddings(tokens)
@@ -34,8 +38,12 @@ Wq = np.random.rand(X_shape, d_k)
 Wk = np.random.rand(X_shape, d_k)
 Wv = np.random.rand(X_shape, d_k)
 
+
 def softmax(arr: np.array, axis=-1):
-    return np.exp(arr - np.max(arr, axis=-1).reshape(-1, 1))/np.sum(np.exp(arr - np.max(arr, axis=-1).reshape(-1, 1)), axis=-1).reshape(-1, 1)
+    max_axis = np.max(arr, axis=-1).reshape(-1, 1)
+    e_x = np.exp(arr - max_axis)
+    return e_x / np.sum(e_x, axis=-1).reshape(-1, 1)
+
 
 def project_qkv(embeddings, d_k=128):
     X = embeddings.detach().numpy()
@@ -45,37 +53,40 @@ def project_qkv(embeddings, d_k=128):
     return Q, K, V
 
 
-
 Q, K, V = project_qkv(embeddings)
 
 
 def plot_mat(matrix: np.array, title=None):
     fig, ax = plt.subplots()
     # 2. Plot the matrix as an image
-    _ = ax.imshow(matrix, cmap='Blues')
+    _ = ax.imshow(matrix, cmap="Blues")
     rows, cols = matrix.shape
     for i in range(rows):
         for j in range(cols):
             # Determine text color based on background intensity for readability
-            text_color = "white" if matrix[i, j] > .60 else "black"
+            text_color = "white" if matrix[i, j] > 0.60 else "black"
             # ax.text(x, y, string, horizontalalignment, verticalalignment)
-            ax.text(j, i, str(matrix[i, j].round(3)), 
-                    ha="center", 
-                    va="center", 
-                    color=text_color,
-                    fontsize=8)
+            ax.text(
+                j,
+                i,
+                str(matrix[i, j].round(3)),
+                ha="center",
+                va="center",
+                color=text_color,
+                fontsize=8,
+            )
     plt.title(title)
     plt.show()
 
 
 # Attention Without dividing by √d
-attn = softmax((Q @ K.T)) 
+attn = softmax((Q @ K.T))
 out = attn @ V
 print((Q @ K.T).std(), (Q @ K.T).mean(), (Q @ K.T).max())
 plot_mat(attn, "attention without dividing by √d")
 
 # Attention after dividing by √d
-attn = softmax((Q @ K.T)/np.sqrt(d_k)) 
+attn = softmax((Q @ K.T) / np.sqrt(d_k))
 out = attn @ V
 plot_mat(attn, "attention after dividing by √d")
 
@@ -85,6 +96,6 @@ tokens = get_tokens(SENTENCE)
 embeddings = get_embeddings(tokens)
 Q, K, V = project_qkv(embeddings)
 
-attn2 = softmax((Q @ K.T)/np.sqrt(d_k)) 
+attn2 = softmax((Q @ K.T) / np.sqrt(d_k))
 out2 = attn2 @ V
 plot_mat(attn2, "attention after changing one token")
---
+++ night_3.py
@@ -0,0 +1,64 @@
+from transformers import AutoModel, AutoTokenizer
+import numpy as np
+
+gpt_model = AutoModel.from_pretrained("gpt2")
+gpt_tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+SENTENCE = "RED HAT HAD"
+tokens = gpt_tokenizer.encode(SENTENCE)
+X = gpt_model.get_input_embeddings().weight[tokens].detach().numpy()
+
+
+def softmax(X, axis=-1):
+    X_max = X.max(axis=axis).reshape(-1, 1)
+    e_x = np.exp(X - X_max)
+    return e_x / e_x.sum(axis=axis).reshape(-1, 1)
+
+
+d_model = 768
+d_k = 128
+n_heads = 2
+input_shape = X.shape[1]
+
+
+class AttnHead:
+    d_head = None
+
+    def __init__(self, d_head):
+        self.d_head = d_head
+        self.Wq = np.random.rand(input_shape, d_head)
+        self.Wk = np.random.rand(input_shape, d_head)
+        self.Wv = np.random.rand(input_shape, d_head)
+
+    def __call__(self, X):
+        Q = X @ self.Wq
+        K = X @ self.Wk
+        V = X @ self.Wv
+        proj = Q @ K.T / np.sqrt(d_head)
+        # print(f"proj shape: {proj.shape}")
+        # print(proj)
+        mask = np.triu(np.ones_like(proj, bool), k=1)
+        proj_masked = np.where(mask, -np.inf, proj)
+        # print(f"proj_mask: {proj_masked.shape}")
+        # print(proj_masked)
+        attn = softmax(proj_masked)
+        # print(f"attn: {attn.shape}")
+        # print(attn, attn.sum(axis=-1))
+        return attn @ V
+
+
+class MultiHeadedAttn:
+    def __init__(self, d_k, n_heads):
+        if d_k % n_heads:
+            raise ValueError("d_k must be divisible by n_heads")
+        self.heads = [AttnHead(d_k // n_heads) for i in range(n_heads)]
+        self.W = np.random.rand(d_k, d_model)
+
+    def __call__(self, X):
+        values = [head(X) for head in self.heads]
+        return np.concat(values, axis=1) @ self.W
+
+
+mha = MultiHeadedAttn(d_k, n_heads)
+v = mha(X)
+print(v.shape)
